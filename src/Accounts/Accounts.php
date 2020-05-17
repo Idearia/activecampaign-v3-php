a99ac8d48ecd374ef5082ba74e48a266557647bc
dunkoh <scott@eurekaranch.com>
2020-05-17 17:43:44 -0400

    add Accounts class

---
+++ src/Accounts/Accounts.php
@@ -0,0 +1,124 @@
+<?php
+
+namespace Mediatoolkit\ActiveCampaign\Accounts;
+
+use Mediatoolkit\ActiveCampaign\Resource;
+
+/**
+ * Class Accounts
+ * @package Mediatoolkit\ActiveCampaign\Accounts
+ * @see https://developers.activecampaign.com/reference#accounts
+ */
+class Accounts extends Resource {
+
+    /**
+     * Create an account
+     * @see https://developers.activecampaign.com/reference#create-an-account
+     *
+     * @param array $account
+     * @return string
+     */
+    public function create(array $account)
+    {
+        $req = $this->client
+            ->getClient()
+            ->post('/api/3/accounts', [
+                'json' => [
+                    'account' => $account
+                ]
+            ]);
+
+        return $req->getBody()->getContents();
+    }
+
+    /**
+     * Get an organization
+     * @see https://developers.activecampaign.com/reference#get-organization
+     *
+     * @param int $id
+     * @return string
+     */
+    public function get(int $id)
+    {
+        $req = $this->client
+            ->getClient()
+            ->get('/api/3/organizations/' . $id);
+
+        return $req->getBody()->getContents();
+    }
+
+    /**
+     * Update an organization
+     * @see https://developers.activecampaign.com/reference#update-organization
+     *
+     * @param int $id
+     * @param array $organization
+     * @return string
+     */
+    public function update(int $id, array $organization)
+    {
+        $req = $this->client
+            ->getClient()
+            ->put('/api/3/organizations/' . $id, [
+                'json' => [
+                    'organization' => $organization
+                ]
+            ]);
+
+        return $req->getBody()->getContents();
+    }
+
+    /**
+     * Delete an organization
+     * @see https://developers.activecampaign.com/reference#delete-organization
+     *
+     * @param int $id
+     * @return string
+     */
+    public function delete(int $id)
+    {
+        $req = $this->client
+            ->getClient()
+            ->delete('/api/3/organizations/' . $id);
+
+        return $req->getBody()->getContents();
+    }
+
+    /**
+     * Delete multiple organizations
+     * @see https://developers.activecampaign.com/reference#delete-multiple-organizations
+     *
+     * @param array $ids
+     * @return string
+     */
+    public function bulkDelete(array $ids)
+    {
+        $req = $this->client
+            ->getClient()
+            ->delete('/api/3/organizations/bulk_delete', [
+                'query' => [
+                    'ids' => $ids
+                ]
+            ]);
+
+        return $req->getBody()->getContents();
+    }
+
+    public function listAll(array $query_params = [], $limit = 20, $offset = 0)
+    {
+        $query_params = array_merge($query_params, [
+            'limit' => $limit,
+            'offset' => $offset
+        ]);
+
+        $req = $this->client
+            ->getClient()
+            ->get('/api/3/accounts', [
+                'query' => $query_params
+            ]);
+
+        return $req->getBody()->getContents();
+    }
+
+
+}
