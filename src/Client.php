a54a952ba112f3c036d7f7aa69703b124049b399
Guido W. Pettinari <guido.pettinari@idearia.it>
2021-09-02 13:18:10 +0200

    Scheletro di retry a livello di Guzzle
    https://github.com/guzzle/guzzle/issues/1806\#issuecomment-293931737

--- src/Client.php
+++ src/Client.php
@@ -2,6 +2,11 @@
 
 namespace Mediatoolkit\ActiveCampaign;
 
+use Throwable;
+
+use GuzzleHttp\Handler\CurlHandler;
+use GuzzleHttp\HandlerStack;
+use GuzzleHttp\Middleware;
 class Client
 {
 
@@ -42,6 +47,20 @@ class Client
     protected $event_tracking_key;
 
     /**
+     * In caso di raggiungimento della quota AC di richieste al secondo,
+     * il client riproverà a lanciare la richiesta per un massimo di $retry
+     * volte.
+     * @var int
+     */
+    protected $retry_times;
+
+    /**
+     * Numero di secondi da aspettare prima di fare un retry.
+     * @var float
+     */
+    protected $retry_delay;
+
+    /**
      * @var \GuzzleHttp\Client
      */
     private $client;
@@ -58,14 +77,24 @@ class Client
         $this->event_tracking_actid = $event_tracking_actid;
         $this->event_tracking_key = $event_tracking_key;
 
-        $this->client = new \GuzzleHttp\Client([
+        $guzzleOptions = [
             'base_uri' => $this->api_url,
             'headers' => [
                 'User-Agent' => self::LIB_USER_AGENT,
                 self::HEADER_AUTH_KEY => $this->api_token,
                 'Accept' => 'application/json'
             ]
-        ]);
+        ];
+
+        // Imposta il retry automatico
+        // https://github.com/guzzle/guzzle/issues/1806#issuecomment-293931737
+        if ($this->retry_times) {
+            $handlerStack = HandlerStack::create(new CurlHandler());
+            $handlerStack->push(Middleware::retry($this->retryDecider(), $this->retryDelay()));
+            $guzzleOptions['handler'] = $handlerStack;
+        }
+
+        $this->client = new \GuzzleHttp\Client(;
 
         if (!is_null($this->event_tracking_actid) && !is_null($this->event_tracking_key)) {
             $this->event_tracking_client = new \GuzzleHttp\Client([
@@ -125,4 +154,73 @@ class Client
         return $this->event_tracking_actid;
     }
 
+    /**
+     * Attiva la modalità retry del client
+     * @return Mediatoolkit\ActiveCampaign\Client
+     */
+    public function withRetry(int $times = 10, float $delay = 0.5)
+    {
+        $this->retry_times = $times;
+        $this->retry_delay = $delay;
+        return $this;
+    }
+
+    /**
+     * Data un'eccezione ritornata dal client, determina
+     * se riprovare o meno a lanciare la richiesta.
+     */
+    // public function retryDecider() // https://github.com/guzzle/guzzle/issues/1806
+    // {
+    //     return function (
+    //         $retries,
+    //         Request $request,
+    //         Response $response = null,
+    //         RequestException $exception = null
+    //     ) {
+    //         // Limit the number of retries to 5
+    //         if ($retries >= 5) {
+    //             return false;
+    //         }
+
+    //         // Retry connection exceptions
+    //         if ($exception instanceof ConnectException) {
+    //             return true;
+    //         }
+
+    //         if ($response) {
+    //             // Retry on server errors
+    //             if ($response->getStatusCode() >= 500 ) {
+    //                 return true;
+    //             }
+    //         }
+
+    //         return false;
+    //     };
+    // }
+    // public function retryDecider( Throwable $th )
+    // {
+    //     if (! $this->retry) {
+    //         return false;
+    //     }
+
+    //     // La richiesta verrà rilanciata nel caso in cui l'errore
+    //     // è dovuto alle troppe richieste al secondo.
+    //     if ( $th->getMessage() === 'piripicchio' || $th->getCode() === 'parapocchio') {
+    //         return true;
+    //     }
+
+    //     return false;
+    // }
+
+    /**
+     * delay 1s 2s 3s 4s 5s
+     *
+     * @return Closure
+     */
+    // public function retryDelay() // https://github.com/guzzle/guzzle/issues/1806
+    // {
+    //     return function ($numberOfRetries) {
+    //         return 1000 * $numberOfRetries;
+    //     };
+    // }
 }
